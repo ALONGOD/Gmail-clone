0a15fcdc322910e968043ed5a0dbd9e7fc7558dc
aidqen <idanmarkin8@gmail.com>
2024-05-23 22:59:31 +0300

    Add MailDetails and finish MailDetailsHeader

--- apps/mail/cmps/MailDetailsHeader.jsx
+++ apps/mail/cmps/MailDetailsHeader.jsx
@@ -1,11 +1,11 @@
-export function MailDetailsHeader({removeMail, onToggleIsRead}) {
+const { useParams, useNavigate } = ReactRouter
 
-
-
+export function MailDetailsHeader({ removeMail, onToggleIsRead }) {
+  const navigate = useNavigate()
   return (
-    <section className="mail-actions flex flex-row">
-      <i className="fa-solid fa-arrow-left"></i>
-      <i className="fa-regular fa-trash-can"></i>
+    <section className="mail-details-header flex flex-row">
+      <i className="fa-solid fa-arrow-left" onClick={() => navigate('/mail')}></i>
+      <i className="fa-regular fa-trash-can" onClick={removeMail}></i>
       <i className="fa-regular fa-envelope" onClick={() => onToggleIsRead(false)}></i>
     </section>
   )
--- apps/mail/views/MailDetails.jsx
+++ apps/mail/views/MailDetails.jsx
@@ -1,73 +1,76 @@
-import { MailDetailsHeader } from "../cmps/MailDetailsHeader.jsx";
-import { MailDetailsMain } from "../cmps/MailDetailsMain.jsx";
-import { MailFilter } from "../cmps/MailFilter.jsx";
-import { mailService } from "../services/mail.service.js";
+import { MailDetailsHeader } from '../cmps/MailDetailsHeader.jsx'
+import { MailDetailsMain } from '../cmps/MailDetailsMain.jsx'
+import { showSuccessMsg, showErrorMsg } from '../../../services/event-bus.service.js'
+import { MailFilter } from '../cmps/MailFilter.jsx'
+import { mailService } from '../services/mail.service.js'
 
 const { useParams, useNavigate } = ReactRouter
 const { useState, useEffect } = React
 
 export function MailDetails() {
-    const [mail, setMail] = useState(null);
-    const [isLoading, setIsLoading] = useState(false);
+  const [mail, setMail] = useState(null)
+  const [isLoading, setIsLoading] = useState(false)
 
-     const { useNavigate } = ReactRouter    
-     const { mailId } = useParams()
+  const { useNavigate } = ReactRouter
+  const { mailId } = useParams()
 
-     const navigate = useNavigate()
+  const navigate = useNavigate()
 
-     
-     useEffect(() => {
-         loadMail()
-        }, [mailId])
-        
-        function loadMail() {
-            setIsLoading(true)
-        mailService.get(mailId)
-        .then(mail => {
-            setMail(mail)
-        })
-        .catch(() => {
-            // showErrorMsg('Couldnt get mail...')
-            navigate('/mail')
-        })
-        .finally(() => {
-            setIsLoading(false)
-        })
-    }
-    
-    function onRemoveMail() {
-        mailService.remove(mailId)
-        .then(() => {
-            showSuccessMsg(`Mail successfully removed! ${mailId}`)
-            navigate('/mail')
-        })
-        .catch(err => {
-            console.log('err:', err)
-            showErrorMsg('Error - mail not removed')
-        })
-    }
-    
-    function onToggleIsRead(isRead) {
-        setMail(prevMail => ({ ...prevMail, isRead: isRead }))
-        mailService.save({ ...mail, isRead: isRead })
-        .then(() => {
-            // showSuccessMsg(`Mail marked as unread! ${mailId}`)
-            navigate('/mail')
-        })
-        .catch(err => {
-            console.log('err:', err)
-            // showErrorMsg('Error - wasn\'t marked as unread')
-        })
-    }
-    
-    console.log(mail);
-    
-    if (!mail) return <div>Loading...</div>
-    return (
-        <main className="mail-details-container">
-            <MailDetailsHeader removeMail={onRemoveMail} onToggleIsRead={onToggleIsRead}/>
-            <MailDetailsMain mail={mail}/>
-        </main>
+  useEffect(() => {
+    loadMail()
+  }, [mailId])
 
-)
+  function loadMail() {
+    setIsLoading(true)
+    mailService
+      .get(mailId)
+      .then(mail => {
+        setMail(mail)
+        showSuccessMsg(`Mail successfully removed! ${mailId}`)
+      })
+      .catch(() => {
+        showErrorMsg('Couldnt get mail...')
+        navigate('/mail')
+      })
+      .finally(() => {
+        setIsLoading(false)
+      })
+  }
+
+  function onRemoveMail() {
+    mailService
+      .remove(mailId)
+      .then(() => {
+        showSuccessMsg(`Mail successfully removed! ${mailId}`)
+        navigate('/mail')
+      })
+      .catch(err => {
+        console.log('err:', err)
+        showErrorMsg('Error - mail not removed')
+      })
+  }
+
+  function onToggleIsRead(isRead) {
+    setMail(prevMail => ({ ...prevMail, isRead: isRead }))
+    mailService
+      .save({ ...mail, isRead: isRead })
+      .then(() => {
+        showSuccessMsg(`Mail marked as unread! ${mailId}`)
+        navigate('/mail')
+      })
+      .catch(err => {
+        console.log('err:', err)
+        showErrorMsg("Error - wasn't marked as unread")
+      })
+  }
+
+  console.log(mail)
+
+  if (!mail) return <div>Loading...</div>
+  return (
+    <main className="mail-details-container">
+      <MailDetailsHeader removeMail={onRemoveMail} onToggleIsRead={onToggleIsRead} />
+      <MailDetailsMain mail={mail} />
+    </main>
+  )
 }
