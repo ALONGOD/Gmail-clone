6f76909ca6b0607278702278e2f53abb4a9c8903
aidqen <idanmarkin8@gmail.com>
2024-05-24 02:37:00 +0300

    Made ComposeBtn cmp and add styling to sidebar-menu

--- apps/mail/cmps/ComposeButton.jsx
+++ apps/mail/cmps/ComposeButton.jsx
@@ -1,4 +1,9 @@
-export function ComposeButton(params) {
-     
-    return <button className="compose-btn">Compose</button>
+export function ComposeButton({ sidebarHover }) {
+  return (
+    <React.Fragment>
+      <div className="compose-btn flex flex-row align-center justify-center">
+        <i className="fa-solid fa-pen"></i> {sidebarHover && <p>Compose</p>}
+      </div>
+    </React.Fragment>
+  )
 }
