1052cf1b0e97d16a096d57a79b83fe1249b2446b
aidqen <idanmarkin8@gmail.com>
2024-05-24 19:08:17 +0300

    Add UserMsg cmp and implement to star toggle

--- RootCmp.jsx
+++ RootCmp.jsx
@@ -1,27 +1,30 @@
 const { Route, Routes } = ReactRouterDOM
 const Router = ReactRouterDOM.HashRouter
 
-import { AppHeader } from "./cmps/AppHeader.jsx"
-import { About } from "./views/About.jsx"
-import { Home } from "./views/Home.jsx"
-import { MailIndex } from "./apps/mail/views/MailIndex.jsx"
-import { NoteIndex } from "./apps/note/views/NoteIndex.jsx"
-import { MailDetails } from "./apps/mail/views/MailDetails.jsx"
-
-
+import { AppHeader } from './cmps/AppHeader.jsx'
+import { About } from './views/About.jsx'
+import { Home } from './views/Home.jsx'
+import { MailIndex } from './apps/mail/views/MailIndex.jsx'
+import { NoteIndex } from './apps/note/views/NoteIndex.jsx'
+import { MailDetails } from './apps/mail/views/MailDetails.jsx'
+import { UserMsg } from './cmps/UserMsg.jsx'
 
 export function App() {
-    return <Router>
-        <section className="app">
-            <AppHeader />
-            <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/about" element={<About />} />
-                <Route path="/mail" element={<MailIndex />} />
-                <Route path="/mail/details/:mailId" element={<MailDetails />} />
+  return (
+    <Router>
+      <section className="app">
+        <AppHeader />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/mail" element={<MailIndex />} />
+          <Route path="/mail/details/:mailId" element={<MailDetails />} />
 
-                <Route path="/note" element={<NoteIndex />} />
-            </Routes>
-        </section>
+          <Route path="/note" element={<NoteIndex />} />
+        </Routes>
+      </section>
+
+      <UserMsg />
     </Router>
+  )
 }
