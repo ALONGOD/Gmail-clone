c5a2612c1884323d151e06aec27e7d3738a748a0
ALONGOD <alongoddard1@gmail.com>
2024-05-24 22:06:36 +0300

    Add routes to note details and edit pages

--- RootCmp.jsx
+++ RootCmp.jsx
@@ -6,6 +6,8 @@ import { About } from './views/About.jsx'
 import { Home } from './views/Home.jsx'
 import { MailIndex } from './apps/mail/views/MailIndex.jsx'
 import { NoteIndex } from './apps/note/views/NoteIndex.jsx'
+import { NoteDetails } from './apps/note/views/NoteDetails.jsx'
+import { NoteEdit } from './apps/note/views/NoteEdit.jsx'
 import { MailDetails } from './apps/mail/views/MailDetails.jsx'
 import { UserMsg } from './cmps/UserMsg.jsx'
 
@@ -21,6 +23,8 @@ export function App() {
           <Route path="/mail/details/:mailId" element={<MailDetails />} />
 
           <Route path="/note" element={<NoteIndex />} />
+          <Route path="/note/edit/:noteId" element={<NoteEdit />} />
+          <Route path="/note/:noteId" element={<NoteDetails />} />
         </Routes>
       </section>
 
