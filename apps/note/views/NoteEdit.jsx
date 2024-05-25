e9716b812f6c45698fd0ef30d3039c8af2578725
ALONGOD <alongoddard1@gmail.com>
2024-05-25 08:33:39 +0300

    Fix class name note bug and add params to note pages

--- apps/note/views/NoteEdit.jsx
+++ apps/note/views/NoteEdit.jsx
@@ -1,5 +1,14 @@
+const { useState, useEffect } = React
+const { useParams, useNavigate } = ReactRouter
 
+const { Link } = ReactRouterDOM
 
 export function NoteEdit() {
-    return <div>Edittt</div>
+    const { noteId } = useParams();
+    return (
+        <div>
+            <h1>Note Details</h1>
+            <p>Note ID: {noteId}</p>
+        </div>
+    );
 }
