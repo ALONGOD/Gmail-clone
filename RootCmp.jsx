25a74245a2f0be90fe7b6cf078b81dd5db9c2907
aidqen <idanmarkin8@gmail.com>
2024-05-26 01:41:24 +0300

    Change routing

--- RootCmp.jsx
+++ RootCmp.jsx
@@ -1,7 +1,6 @@
 const { Route, Routes } = ReactRouterDOM
 const Router = ReactRouterDOM.HashRouter
 
-import { AppHeader } from './cmps/AppHeader.jsx'
 import { About } from './views/About.jsx'
 import { Home } from './views/Home.jsx'
 import { MailIndex } from './apps/mail/views/MailIndex.jsx'
@@ -10,17 +9,22 @@ import { NoteDetails } from './apps/note/views/NoteDetails.jsx'
 import { NoteEdit } from './apps/note/views/NoteEdit.jsx'
 import { MailDetails } from './apps/mail/views/MailDetails.jsx'
 import { UserMsg } from './cmps/UserMsg.jsx'
+import { AppNavigation } from './cmps/AppNavigation.jsx'
+import { MailList } from './apps/mail/cmps/MailList.jsx'
 
 export function App() {
+  // const [mailMainContent, setMailMainContent] = useState('mailList')
   return (
     <Router>
       <section className="app">
-        <AppHeader />
+        <AppNavigation />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
-          <Route path="/mail" element={<MailIndex />} />
-          <Route path="/mail/details/:mailId" element={<MailDetails />} />
+          <Route path="/mail" element={<MailIndex />}>
+            <Route path="/mail/details/:mailId" element={<MailDetails />} />
+            <Route path="/mail/inbox" element={<MailList />} />
+          </Route>
 
           <Route path="/note" element={<NoteIndex />} />
           <Route path="/note/edit/:noteId" element={<NoteEdit />} />
