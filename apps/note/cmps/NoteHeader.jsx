e4c52ad680b0a21e0f1837cb4192c6abbca40db2
ALONGOD <alongoddard1@gmail.com>
2024-05-26 14:15:42 +0300

    Add header note styling

--- apps/note/cmps/NoteHeader.jsx
+++ apps/note/cmps/NoteHeader.jsx
@@ -1,23 +1,25 @@
 import { NoteFilter } from '../cmps/NoteFilter.jsx'
 
 export function NoteHeader({ filterBy, onSetFilterBy }) {
-    return <React.Fragment>
-        <div className="header">
-            <div className="sidebar-header">
-                <img
-                    className="sidebar-menu-btn"
-                    src="assets/img/menu-btn.svg"
-                    onClick={() => setToggleMenu(state => !state)}
-                />
-                <img
-                    className="header-icon"
-                    src="https://uxwing.com/wp-content/themes/uxwing/download/brands-and-social-media/google-keep-icon.png"
-                />
+    return (
+        <React.Fragment>
+            <div className="header">
+                <div className="sidebar-header">
+                    <img
+                        className="sidebar-menu-btn"
+                        src="assets/img/menu-btn.svg"
+                        onClick={() => setToggleMenu(state => !state)}
+                    />
+                    <img
+                        className="header-icon"
+                        src="https://uxwing.com/wp-content/themes/uxwing/download/brands-and-social-media/google-keep-icon.png"
+                    />
+                    <article className="note-sidebar-keep">Keep</article>
+                </div>
+                <div className="search-bar">
+                    <NoteFilter filterBy={filterBy} onSetFilterBy={onSetFilterBy} />
+                </div>
             </div>
-
-            <div className="search-bar">
-                <NoteFilter filterBy={filterBy} onSetFilterBy={onSetFilterBy} />
-            </div>
-        </div>
-    </React.Fragment>
+        </React.Fragment>
+    );
 }
