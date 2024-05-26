56ccd1217f97931b4221d5325f9155a5a0f597e2
aidqen <idanmarkin8@gmail.com>
2024-05-26 16:39:08 +0300

    Adapt the design of the app to gmail more

--- apps/mail/cmps/MailToolsHover.jsx
+++ apps/mail/cmps/MailToolsHover.jsx
@@ -1,11 +1,14 @@
 export function MailToolsHover({ id, isRead, isStar, onRemove, onToggleIsRead, onToggleIsStar }) {
   const isStarClass = isStar ? `star starred fa-solid fa-star ` : `star fa-regular fa-star`
+  const isStarTitle = isStar ? `Starred` : `Not starred`
+  const isReadClass = isRead ? `fa-envelope` :  `fa-envelope-open`
+  const isReadTitle = isRead ? 'unread' : 'read'
 
   return (
     <React.Fragment>
       <i className="delete-btn fa-regular fa-trash-can" title="Remove mail" onClick={() => onRemove(id)}></i>
-      <i className={isStarClass} title="Star mail" onClick={() => onToggleIsStar(id, !isStar)}></i>
-      <i className="fa-regular fa-envelope" title="Un/read mail" onClick={() => onToggleIsRead(id, !isRead)}></i>
+      <i className={isStarClass} title={isStarTitle} onClick={() => onToggleIsStar(id, !isStar)}></i>
+      <i className={`fa-regular ${isReadClass}`} title={`Mark as ${isReadTitle}`} onClick={() => onToggleIsRead(id, !isRead)}></i>
     </React.Fragment>
   )
 }
