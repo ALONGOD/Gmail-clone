b2bbcc9671e5c39ee8f6eec1f14802a271803855
aidqen <idanmarkin8@gmail.com>
2024-05-30 17:24:44 +0300

    Add sorting by read without functionality, just skeleton

--- apps/mail/cmps/MailListActions.jsx
+++ apps/mail/cmps/MailListActions.jsx
@@ -2,8 +2,14 @@ const { useState } = React
 
 export function MailListActions({ readAllEmails }) {
   const [toggleList, setToggleList] = useState(false);
+  const [filterByToEdit, setfilterByToEdit] = useState();
+  
 
   const arrowClass = toggleList ? 'fa-solid fa-caret-up' : 'fa-solid fa-caret-down'
+
+  function handleChange(name, value) {
+    setFilterByToEdit(prevFilterBy => ({ ...prevFilterBy, isRead: value }))
+  }
   
 
   return (
@@ -13,10 +19,9 @@ export function MailListActions({ readAllEmails }) {
         <i onClick={() => setToggleList(state => !state)} className={`arrow-down ${arrowClass} fa-xs`}></i>
       </div>
       {toggleList && <div className="sort-options-container flex flex-column">
-          <h4>All</h4>
-          <h4>Read</h4>
-          <h4>Unread</h4>
-          <h4>Unstarred</h4>
+          <h4 onClick={() => handleChange('')}>All</h4>
+          <h4 onClick={() => handleChange('read')}>Read</h4>
+          <h4 onClick={() => handleChange('unread')}>Unread</h4>
         </div>}
       <i className="mark-all-read fa-regular fa-envelope" title="Mark all read" onClick={readAllEmails}></i>
     </div>
