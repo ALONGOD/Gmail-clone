ae72d06026a5aed668c1a699c82cb19f4f49604c
aidqen <idanmarkin8@gmail.com>
2024-05-30 15:03:55 +0300

    Make sort-options-container

--- apps/mail/cmps/MailListActions.jsx
+++ apps/mail/cmps/MailListActions.jsx
@@ -2,14 +2,22 @@ const { useState } = React
 
 export function MailListActions({ readAllEmails }) {
   const [toggleList, setToggleList] = useState(false);
+
+  const arrowClass = toggleList ? 'fa-solid fa-caret-up' : 'fa-solid fa-caret-down'
   
 
   return (
     <div className="mail-list-actions flex flex-row align-center">
       <div className="mail-list-checkbox-container">
         <input type="checkbox" title="Select all" />
-        <i className="arrow-down fa-solid fa-caret-down fa-xs"></i>
+        <i onClick={() => setToggleList(state => !state)} className={`arrow-down ${arrowClass} fa-xs`}></i>
       </div>
+      {toggleList && <div className="sort-options-container flex flex-column">
+          <h4>All</h4>
+          <h4>Read</h4>
+          <h4>Unread</h4>
+          <h4>Unstarred</h4>
+        </div>}
       <i className="mark-all-read fa-regular fa-envelope" title="Mark all read" onClick={readAllEmails}></i>
     </div>
   )
