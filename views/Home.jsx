27b9983c57ec7e2bea6b69fbbe0f144f232d51f7
aidqen <idanmarkin8@gmail.com>
2024-05-30 23:18:12 +0300

    Make home page design

--- views/Home.jsx
+++ views/Home.jsx
@@ -1,6 +1,14 @@
-
+const { Link } = ReactRouterDOM
 export function Home() {
-    return <section className="home">
-        <h1>Welcome to Appsus!</h1>
+    return <section className="home flex flex-column">
+        <img src="assets/img/homepage-logo.png" />
+        <h1>Appsus!</h1>
+        <h1>The Easiest Way To Manage Your Life</h1>
+        <footer>
+            <Link to="/mail">Email</Link>
+            <Link to="/note">Keep Notes</Link>
+            <Link to="/note">Keep Books</Link>
+            <Link to="/about">About</Link>
+        </footer>
     </section>
 }
