4903c970a840709663d23b833657409ef6c1e2a5
ALONGOD <alongoddard1@gmail.com>
2024-06-01 14:58:54 +0300

    Add pinned notes section

--- apps/note/cmps/NoteList.jsx
+++ apps/note/cmps/NoteList.jsx
@@ -32,56 +32,109 @@ export function NoteList({ notes, onRemove, onPin, onDuplicate, onChangeColor, o
         } else if (note.type === 'NoteTodos') {
             body = note.info.todos.map(todo => `- ${todo.txt}`).join('\n');
         }
-        console.log(subject)
-        console.log(body)
 
         navigate({
             pathname: '/mail',
-            search: `?compose=note&subject=${(subject)}&body=${(body)}`,
+            search: `?compose=note&subject=${subject}&body=${body}`,
         });
     }
 
+    const pinnedNotes = notes.filter(note => note.isPinned);
+    const unpinnedNotes = notes.filter(note => !note.isPinned);
+
     return (
         <div className="note-list">
-            {notes.map(note => {
-                const backgroundColor = note.style && note.style.backgroundColor ? note.style.backgroundColor : '#f8e5c5';
-                return (
-                    <div key={note.id} className='note-card  note-list-item' style={{ backgroundColor }}>
-                        <NotePreview note={note} />
-                        <div className="note-actions">
-                            <button onClick={() => {
-                                if (!note.isTrash) {
-                                    onTrash(note.id);
-                                } else {
-                                    onRemove(note.id);
-                                }
-                            }} className='fa fa-trash'></button>
-                            <button onClick={() => onPin(note.id)} className={`fa ${note.isPinned ? 'fa-thumbtack' : 'fa-map-pin'}`}></button>
-                            <button onClick={() => onDuplicate(note.id)} className='fa fa-clone'></button>
-                            <Link to={`/note/edit/${note.id}`} className="action-button">
-                                <button className='fa fa-edit'></button>
-                            </Link>
-                            <button onClick={() => toggleColorPicker(note.id)} style={{ position: 'relative' }} className='fa fa-paint-brush'></button>
+            {/* Pinned Notes Section */}
+            {pinnedNotes.length > 0 && (
+                <div className="pinned-notes-section">
+                    <h2>Pinned Notes</h2>
+                    {pinnedNotes.map(note => {
+                        const backgroundColor = note.style && note.style.backgroundColor ? note.style.backgroundColor : '#f8e5c5';
+                        return (
+                            <div key={note.id} className='note-card note-list-item' style={{ backgroundColor }}>
+                                <NotePreview note={note} />
+                                <div className="note-actions">
+                                    <button onClick={() => {
+                                        if (!note.isTrash) {
+                                            onTrash(note.id);
+                                        } else {
+                                            onRemove(note.id);
+                                        }
+                                    }} className='fa fa-trash'></button>
+                                    <button onClick={() => onPin(note.id)} className={`fa ${note.isPinned ? 'fa-thumbtack' : 'fa-map-pin'}`}></button>
+                                    <button onClick={() => onDuplicate(note.id)} className='fa fa-clone'></button>
+                                    <Link to={`/note/edit/${note.id}`} className="action-button">
+                                        <button className='fa fa-edit'></button>
+                                    </Link>
+                                    <button onClick={() => toggleColorPicker(note.id)} style={{ position: 'relative' }} className='fa fa-paint-brush'></button>
 
-                            {(note.type === 'NoteTodos' || note.type === 'NoteTxt') && (
-                                <button onClick={() => onSendMail(note)} className='fa fa-envelope'></button>
-                            )}
-                        </div>
-                        {colorPickerVisibility[note.id] && (
-                            <div className='color-palette' style={{ position: 'absolute', top: '0', left: '40px', display: 'flex', flexDirection: 'row' }}>
-                                {colorPalette.map(color => (
-                                    <div
-                                        key={color}
-                                        className='color-option'
-                                        style={{ backgroundColor: color, margin: '0 5px' }}
-                                        onClick={() => handleColorChange(note.id, color)}
-                                    />
-                                ))}
+                                    {(note.type === 'NoteTodos' || note.type === 'NoteTxt') && (
+                                        <button onClick={() => onSendMail(note)} className='fa fa-envelope'></button>
+                                    )}
+                                </div>
+                                {colorPickerVisibility[note.id] && (
+                                    <div className='color-palette' style={{ position: 'absolute', top: '0', left: '40px', display: 'flex', flexDirection: 'row' }}>
+                                        {colorPalette.map(color => (
+                                            <div
+                                                key={color}
+                                                className='color-option'
+                                                style={{ backgroundColor: color, margin: '0 5px' }}
+                                                onClick={() => handleColorChange(note.id, color)}
+                                            />
+                                        ))}
+                                    </div>
+                                )}
                             </div>
-                        )}
-                    </div>
-                );
-            })}
+                        );
+                    })}
+                </div>
+            )}
+
+            {/* Unpinned Notes Section */}
+            {unpinnedNotes.length > 0 && (
+                <div className="unpinned-notes-section">
+                    <h2>Other Notes</h2>
+                    {unpinnedNotes.map(note => {
+                        const backgroundColor = note.style && note.style.backgroundColor ? note.style.backgroundColor : '#f8e5c5';
+                        return (
+                            <div key={note.id} className='note-card note-list-item' style={{ backgroundColor }}>
+                                <NotePreview note={note} />
+                                <div className="note-actions">
+                                    <button onClick={() => {
+                                        if (!note.isTrash) {
+                                            onTrash(note.id);
+                                        } else {
+                                            onRemove(note.id);
+                                        }
+                                    }} className='fa fa-trash'></button>
+                                    <button onClick={() => onPin(note.id)} className={`fa ${note.isPinned ? 'fa-thumbtack' : 'fa-map-pin'}`}></button>
+                                    <button onClick={() => onDuplicate(note.id)} className='fa fa-clone'></button>
+                                    <Link to={`/note/edit/${note.id}`} className="action-button">
+                                        <button className='fa fa-edit'></button>
+                                    </Link>
+                                    <button onClick={() => toggleColorPicker(note.id)} style={{ position: 'relative' }} className='fa fa-paint-brush'></button>
+
+                                    {(note.type === 'NoteTodos' || note.type === 'NoteTxt') && (
+                                        <button onClick={() => onSendMail(note)} className='fa fa-envelope'></button>
+                                    )}
+                                </div>
+                                {colorPickerVisibility[note.id] && (
+                                    <div className='color-palette' style={{ position: 'absolute', top: '0', left: '40px', display: 'flex', flexDirection: 'row' }}>
+                                        {colorPalette.map(color => (
+                                            <div
+                                                key={color}
+                                                className='color-option'
+                                                style={{ backgroundColor: color, margin: '0 5px' }}
+                                                onClick={() => handleColorChange(note.id, color)}
+                                            />
+                                        ))}
+                                    </div>
+                                )}
+                            </div>
+                        );
+                    })}
+                </div>
+            )}
         </div>
     );
 }
