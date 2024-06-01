ec95b677026e196076481c98fed9f929d9abeb14
aidqen <idanmarkin8@gmail.com>
2024-06-01 17:36:00 +0300

    Mobile implementation for mail

--- apps/mail/cmps/MailAppHeader.jsx
+++ apps/mail/cmps/MailAppHeader.jsx
@@ -1,10 +1,10 @@
 import { MailFilter } from './MailFilter'
 
-export function MailAppHeader({ setToggleSidebar }) {
+export function MailAppHeader({ isMobile, setToggleSidebar }) {
   return (
     <div className="email-heading">
       <i className="sidebar-mail-btn fa-solid fa-bars" src="assets/img/menu-btn.svg" onClick={() => setToggleSidebar(state => !state)}></i>
-      <img src="assets/img/mail-img/Gmail_Logo.svg" alt="email-logo" className="email-logo" />
+      {!isMobile && <img src="assets/img/mail-img/Gmail_Logo.svg" alt="email-logo" className="email-logo" />}
     </div>
   )
 }
--- apps/mail/cmps/SidebarMenu.jsx
+++ apps/mail/cmps/SidebarMenu.jsx
@@ -3,7 +3,17 @@ import { SidebarLabel } from './SidebarLabel.jsx'
 const { useState, useEffect } = React
 const { useNavigate } = ReactRouter
 
-export function SidebarMenu({ mailMainContent, setMailMainContent, hoveredSidebar, setSidebarHover, filterBy, onSetFilterBy, unreadMailsCount }) {
+export function SidebarMenu({
+  isMobile,
+  mailMainContent,
+  setMailMainContent,
+  hoveredSidebar,
+  setSidebarHover,
+  setToggleSidebar,
+  filterBy,
+  onSetFilterBy,
+  unreadMailsCount,
+}) {
   const [folderToEdit, setFolderToEdit] = useState({ folder: filterBy.folder })
   const labels = ['inbox', 'starred', 'sent', 'drafts', 'removed']
   const { folder } = filterBy
@@ -19,23 +29,43 @@ export function SidebarMenu({ mailMainContent, setMailMainContent, hoveredSideba
       setMailMainContent('mailList')
     }
     setFolderToEdit({ folder: value })
+    setToggleSidebar(false)
   }
 
   function openNewCompose() {
+    setToggleSidebar(false)
     navigate({
       search: `?compose=new`,
     })
-    setSidebarHover(false)
   }
 
   return (
-    <aside>
-      <section className={`sidebar-menu ${hoveredSidebar ? 'hovered' : ''}`} onMouseEnter={() => setSidebarHover(true)} onMouseLeave={() => setSidebarHover(false)}>
-        <ComposeButton hoveredSidebar={hoveredSidebar} openNewCompose={openNewCompose} />
-        {labels.map(label => {
-          return <SidebarLabel key={label} label={label} folder={folder} handleChange={handleChange} hoveredSidebar={hoveredSidebar} unreadMailsCount={unreadMailsCount} />
-        })}
-      </section>
-    </aside>
+    <React.Fragment>
+      <aside>
+        <section
+          className={`sidebar-menu ${hoveredSidebar ? 'hovered' : ''}`}
+          onMouseEnter={() => setSidebarHover(true)}
+          onMouseLeave={() => setSidebarHover(false)}
+        >
+          <ComposeButton
+            hoveredSidebar={hoveredSidebar}
+            openNewCompose={openNewCompose}
+          />
+          {labels.map(label => {
+            return (
+              <SidebarLabel
+                key={label}
+                label={label}
+                folder={folder}
+                handleChange={handleChange}
+                hoveredSidebar={hoveredSidebar}
+                unreadMailsCount={unreadMailsCount}
+              />
+            )
+          })}
+        </section>
+      </aside>
+      {isMobile && <div className="backdrop" onClick={() => setToggleSidebar(false)}></div>}
+    </React.Fragment>
   )
 }
